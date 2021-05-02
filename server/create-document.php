94dc163feb9508d36a09bf5e0f49552f652e727d
Jefferson Daza <Jefrydaza_206@hotmail.com>
2021-05-02 16:14:37 -0500

    Creacion de documentos termianda

--- server/create-document.php
+++ server/create-document.php
@@ -3,8 +3,11 @@
     include('conection.php');
 
     $nombre =$_POST['nombre'];
+    $contenido =$_POST['contenido'];
+    $tipo="Docuemtno de texto";
+    $carpeta =$_POST['carpeta'];
 
-    $insertQuery = "INSERT INTO carpeta (nombre) values('$nombre')";
+    $insertQuery = "INSERT INTO archivo (nombre, contenido, tipo, carpeta) values('$nombre', '$contenido', '$tipo', '$carpeta')";
     $result = mysqli_query($conection, $insertQuery);
     if($result){
         echo "Creacion exitosa";
---
+++ server/query-document.php
@@ -0,0 +1,16 @@
+<?php
+    header("Acces-Control-Allow-Origin: *");
+    include('conection.php');
+
+    $carpeta=$_POST['carpeta'];
+
+    $insertQuery = "SELECT * FROM archivo where carpeta='$carpeta'";
+    $result = mysqli_query($conection, $insertQuery);
+    while($fila = mysqli_fetch_array($result)){
+        $almacenados[] = array_map('utf8_encode', $fila); 
+    }
+    
+    echo json_encode($almacenados);
+
+    mysqli_close($conection);
+?>
